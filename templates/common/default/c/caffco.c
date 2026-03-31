53a83d9c72677c76047ee73732ba5b61edb4c4ee
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2026-03-31 06:42:51 +0200

    C attempts to compile

--- templates/common/default/c/caffco.c
+++ templates/common/default/c/caffco.c
@@ -13,17 +13,13 @@ caffc_bool caffc_instanceof_check(caffc_ptr o, caffc_u32 ancestor_type_id) {
         return caffc_false;
     }
 
-    if (ancestor_type_id == OBJECT_TYPE_CAFFC_OBJ) {
-        return caffc_true;
-    }
-
     header = (caffc_object_header*) o;
 
     /* check ancestor_type_id bounds? */
     inheritance_data = _caffc_class_headers[header->_caffc_type_id].inheritance_data;
 
-    for (i = 0; i < inheritance_data.count; i++) {
-        if (inheritance_data.implemented_type_ids[i] == ancestor_type_id) {
+    for (i = 0; i < inheritance_data->count; i++) {
+        if (inheritance_data->implemented_type_ids[i] == ancestor_type_id) {
             return caffc_true;
         }
     }
